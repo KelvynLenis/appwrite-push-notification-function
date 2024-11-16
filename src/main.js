03510ebbaa81baf9e2d65102567a8822c6f4169a
KelvynLenis <kmartinslenis30@hotmail.com>
2024-11-16 10:49:36 -0300

    test frontend render

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ export default async ({ req, res, log, error }) => {
     .setProject(process.env.PROJECT_ID)
     .setKey(process.env.API_SECRET_KEY)
 
+  if (req.path === "/") {
+    // Use res object to respond with text(), json(), or binary()
+    // Don't forget to return a response!
+    return res.render("login");
+  }
+
   try {
     const response = await messaging.createPush(
       '23823y4237y472834',
