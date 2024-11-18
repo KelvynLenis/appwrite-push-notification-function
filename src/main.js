e653be12e2b993a0a8fa723f0aa18272e8b7c1e6
KelvynLenis <kmartinslenis30@hotmail.com>
2024-11-18 15:36:32 -0300

    testing

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-import { sendPushNotification } from "./utils";
+import admin from 'firebase-admin';
+
+
+export async function sendPushNotification(payload) {
+}
 
 export default async ({ req, res, log, error }) => {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FCM_PROJECT_ID,
+      clientEmail: process.env.FCM_CLIENT_EMAIL,
+      privateKey: process.env.FCM_PRIVATE_KEY,
+    }),
+    databaseURL: process.env.FCM_DATABASE_URL,
+  });
+
   try {
-    const response = await sendPushNotification({
-      notification: {
-        title: 'req.bodyJson.message.title',
-        body: 'req.bodyJson.message.body',
-      },
-      data: req.bodyJson.data ?? {},
-      token: 'req.bodyJson.deviceToken',
-    });
-
-    log(`Successfully sent message: ${response}`);
-
+    await admin.messaging().send(payload);
     return res.json({ ok: true, messageId: response });
   } catch (e) {
-    error(e);
+    throw "error on messaging ";
     return res.json({ ok: false, error: 'Failed to send the message' }, 500);
   }
+
 };
