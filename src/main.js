907ab611490de6f186619359dfdae470a8eec2ab
KelvynLenis <kmartinslenis30@hotmail.com>
2024-11-18 15:31:14 -0300

    testing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 
 export default async ({ req, res, log, error }) => {
+  try {
+    const response = await sendPushNotification({
+      notification: {
+        title: 'req.bodyJson.message.title',
+        body: 'req.bodyJson.message.body',
+      },
+      data: req.bodyJson.data ?? {},
+      token: 'req.bodyJson.deviceToken',
+    });
 
-  console.log("Deu certo até aqui")
-  return res.json({ ok: true, message: 'Hello World!' });
+    log(`Successfully sent message: ${response}`);
+
+    return res.json({ ok: true, messageId: response });
+  } catch (e) {
+    error(e);
+    return res.json({ ok: false, error: 'Failed to send the message' }, 500);
+  }
 };
