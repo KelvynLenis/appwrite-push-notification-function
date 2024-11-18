ee97774271889df519fdd23896ea246b1ea43b4d
KelvynLenis <kmartinslenis30@hotmail.com>
2024-11-18 12:00:51 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,37 +1,39 @@
 import { throwIfMissing, sendPushNotification } from './utils.js';
 
-throwIfMissing(process.env, [
-  'FCM_PROJECT_ID',
-  'FCM_PRIVATE_KEY',
-  'FCM_CLIENT_EMAIL',
-  'FCM_DATABASE_URL',
-]);
+// throwIfMissing(process.env, [
+//   'FCM_PROJECT_ID',
+//   'FCM_PRIVATE_KEY',
+//   'FCM_CLIENT_EMAIL',
+//   'FCM_DATABASE_URL',
+// ]);
 
 export default async ({ req, res, log, error }) => {
-  try {
-    throwIfMissing(req.body, ['deviceToken', 'message']);
-    throwIfMissing(req.bodyJson.message, ['title', 'body']);
-  } catch (err) {
-    return res.json({ ok: false, error: err.message }, 400);
-  }
+  // try {
+  //   throwIfMissing(req.body, ['deviceToken', 'message']);
+  //   throwIfMissing(req.bodyJson.message, ['title', 'body']);
+  // } catch (err) {
+  //   return res.json({ ok: false, error: err.message }, 400);
+  // }
 
-  log(`Sending message to device: ${req.bodyJson.deviceToken}`);
+  // log(`Sending message to device: ${req.bodyJson.deviceToken}`);
 
-  try {
-    const response = await sendPushNotification({
-      notification: {
-        title: req.bodyJson.message.title,
-        body: req.bodyJson.message.body,
-      },
-      data: req.bodyJson.data ?? {},
-      token: req.bodyJson.deviceToken,
-    });
+  // try {
+  //   const response = await sendPushNotification({
+  //     notification: {
+  //       title: req.bodyJson.message.title,
+  //       body: req.bodyJson.message.body,
+  //     },
+  //     data: req.bodyJson.data ?? {},
+  //     token: req.bodyJson.deviceToken,
+  //   });
 
-    log(`Successfully sent message: ${response}`);
+  //   log(`Successfully sent message: ${response}`);
 
-    return res.json({ ok: true, messageId: response });
-  } catch (e) {
-    error(e);
-    return res.json({ ok: false, error: 'Failed to send the message' }, 500);
-  }
+  //   return res.json({ ok: true, messageId: response });
+  // } catch (e) {
+  //   error(e);
+  //   return res.json({ ok: false, error: 'Failed to send the message' }, 500);
+  // }
+
+  console.log("Deu certo até aqui")
 };
