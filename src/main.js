ea7911826e88405d0a97253a4be0fb7946a02735
KelvynLenis <kmartinslenis30@hotmail.com>
2024-11-24 15:27:55 -0300

    trying to update database

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ export default async ({ req, res, log, error }) => {
   client
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(process.env.APP_WRITE_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-project'] ?? '');
 
   try {
     const { deviceId, isStolen } = JSON.parse(req.body);
